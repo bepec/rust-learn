78683700ae8bac35292d6b265e12421f68c9ead0
Alex Vedmidskyi <loafshock@gmail.com>
2017-10-10 14:26:39 +0200

    hackerrank.com/compare-the-triplet: avoid code duplication (by the cost of intermediate vectors)

--- hackerrank.com/compare-the-triplets/src/main.rs
+++ hackerrank.com/compare-the-triplets/src/main.rs
@@ -1,15 +1,15 @@
+fn read_nums() -> Vec<i32> {
+    let mut input_str = String::new();
+    std::io::stdin().read_line(&mut input_str).expect("Read error.");
+    input_str.split_whitespace()
+             .map(|s| s.parse::<i32>().expect("Failed to parse number."))
+             .collect()
+}
+
 fn main() {
-    let mut alice_str = String::new();
-    std::io::stdin().read_line(&mut alice_str).expect("Read error.");
-    let alice = alice_str.split_whitespace()
-        .map(|s| s.parse::<i32>().expect("Failed to parse number."));
+    let (alice, bob) = (read_nums(), read_nums());
 
-    let mut bob_str = String::new();
-    std::io::stdin().read_line(&mut bob_str).expect("Read error.");
-    let bob = bob_str.split_whitespace()
-        .map(|s| s.parse::<i32>().expect("Failed to parse number."));
-
-    let result = alice.zip(bob)
+    let result = alice.iter().zip(bob.iter())
         .fold((0, 0), |r, (a, b)| {
             if a < b { return (r.0, r.1 + 1); }
             if a > b { return (r.0 + 1, r.1); }
