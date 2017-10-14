bcf55ee275577efc33decfe943d80ca1d2a4b1a2
Alex Vedmidskyi <loafshock@gmail.com>
2017-10-14 21:56:43 +0200

    hackerrank.com/diagonal-difference: avoid unnecessery parsing by using for loop; add test

--- hackerrank.com/diagonal-difference/src/main.rs
+++ hackerrank.com/diagonal-difference/src/main.rs
@@ -2,13 +2,20 @@ fn main() {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
     let n: usize = line.trim().parse().unwrap();
-    let result = (0..n).fold(0, |diff, i| {
+
+    let mut diff = 0;
+    for i in 0..n {
         line.clear();
         std::io::stdin().read_line(&mut line).unwrap();
-        let row = line.split_whitespace()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-        diff + row[i] - row[n - 1 - i]
-    });
-    println!("{}", result.abs());
+        if i * 2 == n - 1 { continue; }
+        let row_diff = line.split_whitespace().enumerate()
+            .filter(|&(j, _)| j == i || j == n - 1 - i)
+            .fold(0, |sum, (j, s)| {
+                let mut val = s.parse::<i32>().unwrap();
+                if i != j { val = -val };
+                sum + val
+            });
+        diff += row_diff;
+    }
+    println!("{}", diff.abs());
 }
